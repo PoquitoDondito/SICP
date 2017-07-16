e1b14c44aaf6bca532f2caccb16847e2c8bc707a
PoquitoDondito <danielchristianstone@gmail.com>
2017-07-15 21:52:21 -0500

    Editing and finishing up to 1.1.7

--- Chapter 1.rkt
+++ Chapter 1.rkt
@@ -1,5 +1,4 @@
 #lang sicp
-(inc 42)
 
 ; 1.1.1
 
@@ -46,3 +45,62 @@ circumference
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
 (f 5)
+
+; 1.1.5
+
+(f 5)
+(sum-of-squares (+ 5 1) (* 5 2))
+(+ (square (+ 5 1))(square (* 5 2)))
+
+; 1.1.6
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+; Alternatively:
+;(define (abs x)
+;  (cond ((< x 0) (- x))
+;        (else x)))
+
+; Alternatively:
+;(define (abs3 x)
+;  (if (< x 0)
+;      (- x)
+;      x))
+
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+; Alternatively:
+;(define (>= x y)
+;  (not (< x y)))
+
+; 1.1.7
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2)(sqrt 3)))
+(square (sqrt 1000))
+
+;1.1.8
+
+;Considering only what these return, they are indistinguishable.
+;(define (square x)(* x x))
+;(define (square x)
+
+
